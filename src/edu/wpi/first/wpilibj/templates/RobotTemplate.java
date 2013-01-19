c7f4b81bf97a3d52c6d3aae8d5385427b107e31c
Cody <1559deviltech@gmail.com>
2013-01-18 19:46:57 -0600

    Added //Hellow
    Random Crap

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
 import edu.wpi.first.wpilibj.Victor;
+//Hellow
 
 public class RobotTemplate extends SimpleRobot{
     Victor vicky;
